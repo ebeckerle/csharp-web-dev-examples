fbff3b5b7e488c8e97e549bf514afbe1d677b0e0
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2023-06-06 08:10:59 -0500

    6/6

--- ReadonlyExample/Temperature/Program.cs
+++ ReadonlyExample/Temperature/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TemperatureExample;
+
 Console.WriteLine("Hello, World!");
+// If the static field is public, we can do this
+Console.WriteLine("Absolute zero in F is: " + Temperature.absoluteZeroFahrenheit);
+
+// If we have an object named "temp" of type Temperature, we cannot do this.
+//Console.WriteLine("Absolute zero in F is: " + temp.absoluteZeroFahrenheit);
